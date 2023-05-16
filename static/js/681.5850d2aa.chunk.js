a99fbb712a249263e99e72a5a3f05a88ff6165b5
Victoria-Panayoti <112144156+Victoria-Panayoti@users.noreply.github.com>
2023-05-16 03:54:47 +0000

    Deploying to gh-pages from @ Victoria-Panayoti/goit-react-hw-05-movies@f38ee1f686b39d242118ef5c495ca818199ebf51 🚀

---
+++ static/js/681.5850d2aa.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[681],{681:function(e,r,t){t.r(r);var a=t(184),s=t(87).useParams;r.default=function(){var e=s().movieId;return(0,a.jsxs)("div",{children:["Reviwes:",e]})}}}]);
+//# sourceMappingURL=681.5850d2aa.chunk.js.map
